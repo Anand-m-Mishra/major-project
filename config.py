ad06675105bef58783792582b4c6815e2303060a
amitesh <amiteshmuduli1@gmail.com>
2025-04-17 01:05:51 +0530

    model prediction

--- config.py
+++ config.py
@@ -12,12 +12,6 @@ class Config:
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16MB max upload size
     
     # Model configuration
-<<<<<<< HEAD
     MODEL_PATH = os.path.join(basedir, 'app', 'models', 'mobilenet_model_88.keras')
-    CLASS_NAMES = ['akiec', 'bcc', 'bkl', 'df', 'mel', 'nv', 'vasc']  # Update with your class names
-    IMAGE_SIZE = (200, 150)  # Should match your model's expected input size
-=======
-    MODEL_PATH = os.path.join(basedir, 'app', 'models', 'SequentialCNN_DualLayer2_model_66.keras')
     CLASS_NAMES = ['Melanocytic nevi', 'Melanoma', 'Benign keratosis-like lesions', 'Basal cell carcinoma', 'Actinic keratoses', 'Vascular lesions', 'Dermatofibroma', "Normal skin"]  # Example skin lesion classes
     IMAGE_SIZE = (200, 150)  # Adjust based on your model's expected input size
->>>>>>> cfa8a89c7b07f87e1f3ff18a437758336b64ef62
